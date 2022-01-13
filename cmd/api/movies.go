a9a95d408566c803b227e92737f0efa3865781c6
Katarzyna Kawala <kat.kawala@gmail.com>
2022-01-13 08:38:00 +0100

    unmarshaling runtime value

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -10,10 +10,10 @@ import (
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
 	var input struct {
-		Title string `json:"title"`
-		Year int32 `json:"year"`
-		Runtime int32 `json:"runtime"`
-		Genres []string `json:"genres"`
+		Title   string       `json:"title"`
+		Year    int32        `json:"year"`
+		Runtime data.Runtime `json:"runtime"`
+		Genres  []string     `json:"genres"`
 	}
 
 	err := app.readJSON(w, r, &input)
@@ -33,12 +33,12 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 	}
 
 	movie := data.Movie{
-		ID: 		id, 
-		CreatedAt: 	time.Now(),
-		Title: 		"Casablance",
-		Runtime: 	102,
-		Genres: 	[]string{"drama", "romance", "war"},
-		Version: 	1,
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Casablance",
+		Runtime:   102,
+		Genres:    []string{"drama", "romance", "war"},
+		Version:   1,
 	}
 
 	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
