89d7478e1b0528e860b1e6e0db7e7c4dcf2daf03
Katarzyna Kawala <kat.kawala@gmail.com>
2022-01-17 19:28:00 +0100

    getting movie from db by id

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
-	"time"
 
 	"movieapp.github.com.katarzynakawala/internal/data"
 	"movieapp.github.com.katarzynakawala/internal/validator"
@@ -61,13 +61,15 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
-	movie := data.Movie{
-		ID:        id,
-		CreatedAt: time.Now(),
-		Title:     "Casablance",
-		Runtime:   102,
-		Genres:    []string{"drama", "romance", "war"},
-		Version:   1,
+	movie, err := app.models.Movies.Get(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundResponse(w,r)
+		default:
+			app.serverErrorResponse(w, r, err)	 
+		}
+		return
 	}
 
 	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
