bc097f452498732b7fa0426ab2ff9a59ee12efaf
Katarzyna Kawala <kat.kawala@gmail.com>
2022-01-21 22:56:01 +0100

    adding full-text search functionalities

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -161,8 +161,8 @@ func (m MovieModel) GetAll(title string, genres []string, filters Filters) ([]*M
 	query := `
 		SELECT id, created_at, title, year, runtime, genres, version
 		FROM movies
-		WHERE (LOWER(title) = LOWER($1) OR $1 = '')
-		AND (genres @> $2 OR $2 = '{}')
+		WHERE (to_tsvector('simple', title) @@ plainto_tsquery('simple', $1) OR $1 = '') 
+		AND (genres @> $2 OR $2 = '{}')     
 		ORDER BY id`
 	
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
