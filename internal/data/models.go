51a72b525936a559a553211e84d20bf6f6237115
Katarzyna Kawala <kat.kawala@gmail.com>
2022-01-26 21:46:10 +0100

    adding user model

--- internal/data/models.go
+++ internal/data/models.go
@@ -14,10 +14,12 @@ var (
 //Model wraps MovieModel
 type Models struct {
 	Movies MovieModel
+	Users  UserModel
 }
 
 func NewModels(db *sql.DB) Models {
 	return Models{
 		Movies: MovieModel{DB: db},
+		Users:  UserModel{DB: db},
 	}
 }
