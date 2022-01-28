7c57bdbd6480f2f32199f782cdee66e5e7d0f6ac
Katarzyna Kawala <kat.kawala@gmail.com>
2022-01-28 22:50:25 +0100

    sending token to the user

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"net/http"
+	"time"
 
 	"movieapp.github.com.katarzynakawala/internal/data"
 	"movieapp.github.com.katarzynakawala/internal/validator"
@@ -32,7 +33,7 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 		app.serverErrorResponse(w, r, err)
 		return
 	}
-	
+
 	v := validator.New()
 
 	if data.ValidateUser(v, user); !v.Valid() {
@@ -47,14 +48,24 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 			v.AddError("email", "a user with this email address already exists")
 			app.failedValidationResponse(w, r, v.Errors)
 		default:
-			app.serverErrorResponse(w, r, err)	
+			app.serverErrorResponse(w, r, err)
 		}
 		return
 	}
 
-	
+	token, err := app.models.Tokens.New(user.ID, 3*24*time.Hour, data.ScopeActivation)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
 	app.background(func() {
-		err = app.mailer.Send(user.Email, "user_welcome.tmpl", user) 
+		data := map[string]interface{}{
+			"activationToken": token.Plaintext,
+			"userID":          user.ID,
+		}
+
+		err = app.mailer.Send(user.Email, "user_welcome.tmpl", data)
 		if err != nil {
 			app.logger.PrintError(err, nil)
 		}
