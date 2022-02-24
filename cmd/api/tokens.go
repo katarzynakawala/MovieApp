0dc8d267d6f8929e6b7a68c85f146529816eb83e
Katarzyna Kawala <kat.kawala@gmail.com>
2022-02-24 21:16:18 +0100

    adding handler for changing password

--- cmd/api/tokens.go
+++ cmd/api/tokens.go
@@ -64,3 +64,43 @@ func (app *application) createAuthenticationTokenHandler(w http.ResponseWriter, 
 		app.serverErrorResponse(w, r, err)
 	}
 }
+
+func (app *application) createPasswordResetTokenHandler(w http.ResponseWriter, r *http.Request) {
+	var input struct {
+		Email string `json:"email"`
+	}
+
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	v:= validator.New()
+
+	if data.ValidateEmail(v, input.Email); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	user, err := app.models.Users.GetByEmail(input.Email)
+    if err != nil {
+        switch {
+        case errors.Is(err, data.ErrRecordNotFound):
+            v.AddError("email", "no matching email address found")
+            app.failedValidationResponse(w, r, v.Errors)
+        default:
+            app.serverErrorResponse(w, r, err)
+        }
+        return
+    }
+
+	if !user.Activated {
+        v.AddError("email", "user account must be activated")
+        app.failedValidationResponse(w, r, v.Errors)
+        return
+    }
+
+
+
+}
