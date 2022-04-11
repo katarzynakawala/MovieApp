61723637706bc259116e0633e813ea1243f3544e
Katarzyna Kawala <kat.kawala@gmail.com>
2022-04-11 21:51:08 +0200

    adding test of unauthorized display of the movie

--- cmd/api/movies_test.go
+++ cmd/api/movies_test.go
@@ -7,12 +7,28 @@ import (
 
 var unauthorizedBody string = "{\n\t\"error\": \"you must be authenticated to access this resource\"\n}\n"
 
-func TestListMoives(t *testing.T) {
+func TestListMovies(t *testing.T) {
 	app := newTestApplication(t)
  	ts := newTestServer(t, app.routes())
  	defer ts.Close()
 
  	code, _, body := ts.get(t, "/v1/movies")
+	
+	if code != http.StatusUnauthorized {
+        t.Errorf("want %d; got %d", http.StatusUnauthorized, code)
+    }
+
+    if string(body) != unauthorizedBody {
+         t.Errorf("want body to equal %q; got %q", unauthorizedBody, body)
+	} 
+}
+
+func TestShowMovie(t *testing.T) {
+	app := newTestApplication(t)
+ 	ts := newTestServer(t, app.routes())
+ 	defer ts.Close()
+
+ 	code, _, body := ts.get(t, "/v1/movies/3")
 	
 	if code != http.StatusUnauthorized {
         t.Errorf("want %d; got %d", http.StatusUnauthorized, code)
